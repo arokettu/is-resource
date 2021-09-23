411643433c789258ad42aa66dd156758fb68d5a1
Anton Smirnov <sandfox@sandfox.me>
2021-09-24 02:00:00 +0300

    Test that class name with extension not loaded is not considered a resource

--- tests/assert.php
+++ tests/assert.php
@@ -1,8 +1,46 @@
 <?php
 
-function assert_true($value, $message = null)
+class TestAssertion extends Exception {}
+
+function assert_equals($expected, $actual, $message = null)
 {
-    if ($value !== true) {
-        throw new RuntimeException($message ?: 'Failed to assert that value is true');
+    if ($expected !== $actual) {
+        throw new TestAssertion($message ?: 'Failed to assert that expected is equal to actual');
+    }
+}
+
+function assert_true($actual, $message = null)
+{
+    assert_equals(true, $actual, $message ?: 'Failed to assert that value is true');
+}
+
+function assert_false($actual, $message = null)
+{
+    assert_equals(false, $actual, $message ?: 'Failed to assert that value is false');
+}
+
+function assert_null($actual, $message = null)
+{
+    assert_equals(null, $actual, $message ?: 'Failed to assert that value is null');
+}
+
+function assert_exception(\Closure $callback, $exceptionClass, $exceptionMessage = null, $message = null)
+{
+    $message = $message ?: 'Failed to assert that the expected exception is thrown';
+
+    try {
+        $callback();
+    } catch (Exception $e) {
+        assert_equals($exceptionClass, get_class($e), $message);
+
+        if ($exceptionMessage) {
+            assert_equals($exceptionMessage, $e->getMessage(), $message);
+        }
+    } catch (Throwable $e) {
+        assert_equals($exceptionClass, get_class($e), $message);
+
+        if ($exceptionMessage) {
+            assert_equals($exceptionMessage, $e->getMessage(), $message);
+        }
     }
 }
