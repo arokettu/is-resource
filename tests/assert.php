e3cc5ba36a6cd5cc1095653f4ef53b03bfc750d3
Anton Smirnov <sandfox@sandfox.me>
2021-09-24 02:57:49 +0300

    Actual tests

--- tests/assert.php
+++ tests/assert.php
@@ -5,28 +5,34 @@ class TestAssertion extends Exception {}
 function assert_equals($expected, $actual, $message = null)
 {
     if ($expected !== $actual) {
-        throw new TestAssertion($message ?: 'Failed to assert that expected is equal to actual');
+        throw new TestAssertion(
+            $message ?: sprintf(
+                'Failed to assert that %s is equal to %s',
+                json_encode($expected),
+                json_encode($actual)
+            )
+        );
     }
 }
 
 function assert_true($actual, $message = null)
 {
-    assert_equals(true, $actual, $message ?: 'Failed to assert that value is true');
+    assert_equals(true, $actual, $message);
 }
 
 function assert_false($actual, $message = null)
 {
-    assert_equals(false, $actual, $message ?: 'Failed to assert that value is false');
+    assert_equals(false, $actual, $message);
 }
 
 function assert_null($actual, $message = null)
 {
-    assert_equals(null, $actual, $message ?: 'Failed to assert that value is null');
+    assert_equals(null, $actual, $message);
 }
 
 function assert_exception(\Closure $callback, $exceptionClass, $exceptionMessage = null, $message = null)
 {
-    $message = $message ?: 'Failed to assert that the expected exception is thrown';
+    $message = $message ?: sprintf('Failed to assert that the expected exception %s is thrown', $exceptionClass);
 
     try {
         $callback();
--- tests/tests.php
+++ tests/tests.php
@@ -2,3 +2,19 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/assert.php';
+
+$testCases = array();
+
+$testCases[] = array('gd', function() { return imagecreate(100, 100); });
+$testCases[] = array('GMP integer', function() { return gmp_init('1'); });
+$testCases[] = array('Hash Context', function() { return hash_init('md5'); });
+
+foreach ($testCases as $testCase) {
+    list($resourceType, $callback) = $testCase;
+
+    $resource = $callback();
+
+    assert_true(\Arokettu\IsResource\is_resource($resource));
+    assert_equals($resourceType, \Arokettu\IsResource\get_resource_type($resource));
+    assert_equals($resourceType, \Arokettu\IsResource\try_get_resource_type($resource));
+}
