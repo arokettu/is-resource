36a0f3ac8d84a03471d7a18efeec8aa8458dae7e
Anton Smirnov <sandfox@sandfox.me>
2021-09-24 04:02:41 +0300

    Rebuild

--- gen/ResourceMap80000.php
+++ gen/ResourceMap80000.php
@@ -14,12 +14,24 @@ final class ResourceMap80000
     public static function map()
     {
         return array(
+            'AddressInfo' => array('sockets', 'AddressInfo'),
             'CurlHandle' => array('curl', 'curl'),
             'CurlMultiHandle' => array('curl', 'curl_multi'),
             'CurlShareHandle' => array('curl', 'curl_share'),
+            'DeflateContext' => array('zlib', 'zlib.deflate'),
+            'EnchantBroker' => array('enchant', 'enchant_broker'),
+            'EnchantDictionary' => array('enchant', 'enchant_dict'),
             'GMP' => array('gmp', 'GMP integer'),
             'GdImage' => array('gd', 'gd'),
             'HashContext' => array('hash', 'Hash Context'),
+            'InflateContext' => array('zlib', 'zlib.inflate'),
+            'Shmop' => array('shmop', 'shmop'),
+            'Socket' => array('sockets', 'Socket'),
+            'SysvMessageQueue' => array('sysvmsg', 'sysvmsg queue'),
+            'SysvSemaphore' => array('sysvsem', 'sysvsem'),
+            'SysvSharedMemory' => array('sysvshm', 'sysvshm'),
+            'XMLParser' => array('xml', 'xml'),
+            'XMLWriter' => array('xmlwriter', 'xmlwriter'),
         );
     }
 }
