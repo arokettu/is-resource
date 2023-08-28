59db6a592acceb8b757c3be2a030dfb1b75e874a
Anton Smirnov <sandfox@sandfox.me>
2023-08-28 12:53:45 +0300

    Import funcs and constants

--- data/ResourceMapLoader.tpl.php
+++ data/ResourceMapLoader.tpl.php
@@ -8,15 +8,15 @@
 namespace Arokettu\IsResource;
 
 <?php foreach ($versions as $version): ?>
-if (PHP_VERSION_ID >= <?php echo $version ?>) {
+if (\PHP_VERSION_ID >= <?php echo $version ?>) {
     require __DIR__ . '/ResourceMap<?php echo $version ?>.php';
-    class_alias('Arokettu\\IsResource\\ResourceMap<?php echo $version ?>', 'Arokettu\\IsResource\\ResourceMap');
+    \class_alias('Arokettu\\IsResource\\ResourceMap<?php echo $version ?>', 'Arokettu\\IsResource\\ResourceMap');
     return;
 }
 
 <?php endforeach; ?>
 require __DIR__ . '/ResourceMap50000.php';
-class_alias('Arokettu\\IsResource\\ResourceMap50000', 'Arokettu\\IsResource\\ResourceMap');
+\class_alias('Arokettu\\IsResource\\ResourceMap50000', 'Arokettu\\IsResource\\ResourceMap');
 
 if (\false) {
     /**
--- gen/ResourceMap.php
+++ gen/ResourceMap.php
@@ -2,32 +2,32 @@
 
 namespace Arokettu\IsResource;
 
-if (PHP_VERSION_ID >= 80100) {
+if (\PHP_VERSION_ID >= 80100) {
     require __DIR__ . '/ResourceMap80100.php';
-    class_alias('Arokettu\\IsResource\\ResourceMap80100', 'Arokettu\\IsResource\\ResourceMap');
+    \class_alias('Arokettu\\IsResource\\ResourceMap80100', 'Arokettu\\IsResource\\ResourceMap');
     return;
 }
 
-if (PHP_VERSION_ID >= 80000) {
+if (\PHP_VERSION_ID >= 80000) {
     require __DIR__ . '/ResourceMap80000.php';
-    class_alias('Arokettu\\IsResource\\ResourceMap80000', 'Arokettu\\IsResource\\ResourceMap');
+    \class_alias('Arokettu\\IsResource\\ResourceMap80000', 'Arokettu\\IsResource\\ResourceMap');
     return;
 }
 
-if (PHP_VERSION_ID >= 70200) {
+if (\PHP_VERSION_ID >= 70200) {
     require __DIR__ . '/ResourceMap70200.php';
-    class_alias('Arokettu\\IsResource\\ResourceMap70200', 'Arokettu\\IsResource\\ResourceMap');
+    \class_alias('Arokettu\\IsResource\\ResourceMap70200', 'Arokettu\\IsResource\\ResourceMap');
     return;
 }
 
-if (PHP_VERSION_ID >= 50600) {
+if (\PHP_VERSION_ID >= 50600) {
     require __DIR__ . '/ResourceMap50600.php';
-    class_alias('Arokettu\\IsResource\\ResourceMap50600', 'Arokettu\\IsResource\\ResourceMap');
+    \class_alias('Arokettu\\IsResource\\ResourceMap50600', 'Arokettu\\IsResource\\ResourceMap');
     return;
 }
 
 require __DIR__ . '/ResourceMap50000.php';
-class_alias('Arokettu\\IsResource\\ResourceMap50000', 'Arokettu\\IsResource\\ResourceMap');
+\class_alias('Arokettu\\IsResource\\ResourceMap50000', 'Arokettu\\IsResource\\ResourceMap');
 
 if (\false) {
     /**
